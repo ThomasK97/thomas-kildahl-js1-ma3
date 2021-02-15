da39a87822c84e957280c0bf3576a03c1fe8ef18
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-02-15 12:19:38 +0100

    updated

--- js/question-1.js
+++ js/question-1.js
@@ -3,7 +3,7 @@ console.log("logged in script 1");
 
 
 
-const division = (a,b)
-=> {
-    return a % b
-}
+const division = (a, b) => {
+    a % b;
+};
+
--- js/question-2.js
+++ js/question-2.js
@@ -1,1 +1,38 @@
 console.log("logged in script 2");
+
+const loading = document.querySelector(".loader");
+
+setTimeout(function(){
+    loading.innerHTML = "Done Loading!";
+}, 3000);
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const outPut = document.querySelector(".games");
+
+async function allGames(){
+  try {
+    const response = await fetch(url);
+    const list = await response.json();
+    const games = list.results;
+    
+    outPut.innerHTML = "";
+
+
+    for (let i = 0; i <= games.length; i++){
+      if (i===8) {
+        break;
+      } 
+      
+      
+      outPut.innerHTML += `<div class= "outPutResult"> <h1>${games[i].name}</h1> <br></br> <p>Rating:${games[i].rating}</p> <br></br> <p>Tag:${games[i].tags.length}</p> </div>`;
+    }
+  } catch (error) {
+    outPut.innerHTML = error;  
+  } 
+}
+
+allGames();
+            
+
+   
